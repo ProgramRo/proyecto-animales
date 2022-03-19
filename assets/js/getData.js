c9c0f1d8157627ac6bf956871249b66584257853
Rodrigo Palza Gonzalez <98556305+ProgramRo@users.noreply.github.com>
2022-03-19 11:55:48 -0300

    Update getData.js

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:5500/animales.json"
+const url = "https://programro.github.io/proyecto-animales/animales.json"
 const getData = async () => {
     const res = await fetch(url)
     const data = await res.json()
